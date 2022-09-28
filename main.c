03c048945c416df168723759470e2207a0ccd2fc
MatteoPT <matteofioreasl@gmail.com>
2022-09-28 09:43:28 +0200

    Opening every /proc/[PID] directory

--- main.c
+++ main.c
@@ -4,6 +4,15 @@
 #include<dirent.h>
 #include<string.h>
 
+char* getDirectory(char* pid) {
+  char* res = (char*) malloc(sizeof("/proc") + strlen(pid) + 1);
+  strcpy(res, "/proc");
+  strcat(res, "/");
+  strcat(res, pid);
+  return res;
+}
+
+
 int main() {
 
   DIR* dir;
@@ -25,7 +34,18 @@ int main() {
   printf("IN THREAD\n");
   //Una volta nel thread, stampo i PID di tutti i processi
   while( (proc_r = readdir(dir)) != NULL) {
-    printf("pid -> %s\n", proc_r->d_name);
+  
+    printf("pid -> %s\n", proc_r->d_name);    
+    char* directory = getDirectory(proc_r->d_name);
+    
+    DIR* pid_dir;
+    if( (pid_dir = opendir(directory)) == NULL) {
+      //Gestione errori
+      printf("Errore nell'apertura di %s\n", directory);
+      exit(EXIT_FAILURE);
+    }
+  
   }
+  
   
 }
